5c03dbbb931b572179f0316a2630813b53bac882
Jamie Bullock <jamie@jamiebullock.com>
2013-07-23 19:19:01 +0100

    Add TestFlight

--- Tape/Tape/AppDelegate.m
+++ Tape/Tape/AppDelegate.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2013 Jamie Bullock. All rights reserved.
 //
 
+
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
@@ -14,12 +15,13 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    // Override point for customization after application launch.
+
+-(BOOL)application:(UIApplication *)application
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [TestFlight takeOff:@"0c8fcfd8-b7d8-4526-8cab-dcf93c8a89d8"];
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
